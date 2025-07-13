7d386861f1df9d706cf9c3a0a4e6356b4cd579d9
thebuilderekes <ekeopreberedugo1@gmail.com>
2025-07-13 10:06:10 +0100

    improve nav functionality and footer

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const nav = document.querySelector("nav");
+const navLinks = document.querySelectorAll("nav ul li a");
 const navToggleBtn = document.querySelector(".nav-toggler");
 const navMobileToggleBtn = document.querySelector("nav ul .close-nav-btn");
-
 const isOpen = navToggleBtn.getAttribute("aria-expanded") === "true";
 
+//toggle nav with nav toggle button
 navToggleBtn.addEventListener("click", function() {
-
   if (nav.classList.contains("show")) {
     nav.classList.remove("show");
     nav.classList.add("hide");
@@ -20,6 +20,7 @@ navToggleBtn.addEventListener("click", function() {
   }
 });
 
+// make mobile toggle button hide nav 
 navMobileToggleBtn.addEventListener("click", function() {
   nav.classList.remove("show");
   nav.classList.add("hide");
@@ -28,3 +29,24 @@ navMobileToggleBtn.addEventListener("click", function() {
     navMobileToggleBtn.setAttribute("aria-expanded", false)
   }
 })
+
+// make nav hide on click of nav links
+if (window.innerWidth < 768) {
+  navLinks.forEach(link => {
+    link.addEventListener("click", function() {
+      nav.classList.remove("show");
+      nav.classList.add("hide");
+      if (nav.classList.contains("hide")) {
+        navToggleBtn.setAttribute("aria-expanded", false)
+        navMobileToggleBtn.setAttribute("aria-expanded", false)
+      }
+
+    })
+
+  });
+}
+
+
+
+
+
